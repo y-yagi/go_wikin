ba96c85a90f74366818cedf9a0b022e078ce08a9
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2015-08-01 10:15:57 +0900

    first commit

---
+++ wikin_client.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"encoding/base64"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+)
+
+const (
+	url = "https://wikin.herokuapp.com/pages.json"
+)
+
+func authInfo() string {
+	user := os.Getenv("WIKIN_USER")
+	password := os.Getenv("WIKIN_PASSWORD")
+	return base64.StdEncoding.EncodeToString([]byte(user + ":" + password))
+}
+
+func get() {
+	req, _ := http.NewRequest("GET", url, nil)
+	req.Header.Set("Authorization", "Basic "+authInfo())
+
+	client := new(http.Client)
+	resp, err := client.Do(req)
+
+	if err != nil {
+		fmt.Println("connect error\n")
+		fmt.Println(err)
+		return
+	}
+
+	byteArray, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println("read error\n")
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Println(string(byteArray))
+}
+
+func main() {
+	get()
+}
