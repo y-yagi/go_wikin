0dc72caf20a343a7db31ee7ee0d29b374c301aeb
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2015-08-01 11:05:59 +0900

    add parse operation

--- wikin_client.go
+++ wikin_client.go
@@ -3,7 +3,9 @@ package main
 import (
 	"encoding/base64"
 	"fmt"
+	"github.com/bitly/go-simplejson"
 	"io/ioutil"
+	"log"
 	"net/http"
 	"os"
 )
@@ -12,13 +14,17 @@ const (
 	url = "https://wikin.herokuapp.com/pages.json"
 )
 
+type Page struct {
+	Id, Url, Title, Body string
+}
+
 func authInfo() string {
 	user := os.Getenv("WIKIN_USER")
 	password := os.Getenv("WIKIN_PASSWORD")
 	return base64.StdEncoding.EncodeToString([]byte(user + ":" + password))
 }
 
-func get() string {
+func get() []byte {
 	req, _ := http.NewRequest("GET", url, nil)
 	req.Header.Set("Authorization", "Basic "+authInfo())
 
@@ -26,21 +32,36 @@ func get() string {
 	resp, err := client.Do(req)
 
 	if err != nil {
-		fmt.Println("connect error\n")
-		fmt.Println(err)
-		return ""
+		log.Fatal(err)
 	}
 
 	byteArray, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		fmt.Println("read error\n")
-		fmt.Println(err)
-		return ""
+		log.Fatal(err)
 	}
 
-	return string(byteArray)
+	return byteArray
+}
+
+func parse(jsonString []byte) []interface{} {
+	js, err := simplejson.NewJson(jsonString)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	array, err := js.Get("pages").Array()
+	if err != nil {
+		log.Fatal(err)
+	}
+	return array
 }
 
 func main() {
-	get()
+	jsonString := get()
+	pages := parse(jsonString)
+
+	for _, page := range pages {
+		fmt.Println(page)
+	}
 }
